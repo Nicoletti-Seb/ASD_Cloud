b221d2f089bcf3c915a7bd77b1aab5c3e9952446
Nicoletti Sébastien <s.nico@live.fr>
2015-11-29 16:29:52 +0100

    MasterMain: Création du main pour lancer un Master.

--- src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
@@ -1,16 +1,33 @@
 package fr.unice.miage.sd.tinydfs.main;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import fr.unice.miage.sd.tinydfs.nodes.Master;
+import fr.unice.miage.sd.tinydfs.nodes.MasterImpl;
 
 public class MasterMain {
-
+	
 	// Usage: java fr.unice.miage.sd.tinydfs.main.MasterMain storage_service_name dfs_root_folder nb_slaves
-	public static void main(String[] args) {
-		System.out.println("Test !!!!");
-		/*String storageServiceName = args[0];
+	public static void main(String[] args) {		
+		String storageServiceName = args[0];
 		String dfsRootFolder = args[1];
-		int nbSlaves = Integer.parseInt(args[2]);*/
+		int nbSlaves = Integer.parseInt(args[2]);
 		
 		// Create master and register it
+		try {
+			Master master = new MasterImpl(nbSlaves, dfsRootFolder);
+			Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+			registry.bind( storageServiceName, master);
+			System.out.println("Server ready : " + storageServiceName);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (AlreadyBoundException e) {
+			e.printStackTrace();
+		}
+		
 
 	}
 	
