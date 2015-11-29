c3c151074c57bdf28e7441ba81323c9a9cb78ab9
Nicoletti Sébastien <s.nico@live.fr>
2015-11-29 17:48:55 +0100

    MasterImpl: Changement dans la gestion des fichiers pour la partie reconstruction.
    - Sauvegarde des blocs mémoires dans des fichiers séparés avant la
    reconstruction.
    - Lecture des blocs mémoires à partir des fichiers pour la
    reconstruction.
    
    -->Les tests de "ClientsTest" fonctionnent avec 2 slaves.

--- src/main/java/fr/unice/miage/sd/tinydfs/nodes/MasterImpl.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/nodes/MasterImpl.java
@@ -8,10 +8,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -68,7 +69,7 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 		int overflow = (int) (file.length() % slaves.length);
 		BufferedInputStream bis = null;
 		byte[] buffer;
-		
+
 		try {
 			bis = new BufferedInputStream(new FileInputStream(file));
 			for (byte indexPart = 0; bis.available() > 0; indexPart++) {
@@ -105,7 +106,8 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 	}
 
 	/*
-	 * @see fr.unice.miage.sd.tinydfs.nodes.Master#saveBytes(java.lang.String, byte[])
+	 * @see fr.unice.miage.sd.tinydfs.nodes.Master#saveBytes(java.lang.String,
+	 * byte[])
 	 */
 	@Override
 	public void saveBytes(String filename, byte[] fileContent) throws RemoteException {
@@ -113,29 +115,28 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 		List<byte[]> subFileContentLeft = new ArrayList<>();
 		int sizePart = (int) (fileContent.length / slaves.length);
 		int overflow = (int) (fileContent.length % slaves.length);
-		byte [] buffer = null;
-		
+		byte[] buffer = null;
+
 		for (byte indexPart = 0; indexPart < slaves.length; indexPart++) {
-			if( indexPart == 0 ){
-				buffer = new byte [sizePart + overflow + 1];
+			if (indexPart == 0) {
+				buffer = new byte[sizePart + overflow + 1];
 				System.arraycopy(fileContent, 0, buffer, 1, sizePart + overflow);
-			}
-			else{
-				buffer = new byte[sizePart + 1 ];
+			} else {
+				buffer = new byte[sizePart + 1];
 				int indexSrc = indexPart * sizePart;
 				System.arraycopy(fileContent, indexSrc, buffer, 1, sizePart);
 			}
-			
+
 			buffer[0] = indexPart;
-			
+
 			if (indexPart % 2 == 0) {
 				subFileContentLeft.add(buffer);
 			} else {
 				subFileContentRight.add(buffer);
 			}
-			
+
 		}
-		
+
 		slaves[0].subSave(filename, subFileContentLeft);
 		slaves[1].subSave(filename, subFileContentRight);
 	}
@@ -148,53 +149,96 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 	public File retrieveFile(String filename) throws RemoteException {
 		List<byte[]> subFileContentLeft = slaves[0].subRetrieve(filename);
 		List<byte[]> subFileContentRight = slaves[1].subRetrieve(filename);
-		
-		if( subFileContentLeft.isEmpty() || subFileContentRight.isEmpty() ){
+
+		if (subFileContentLeft.isEmpty() || subFileContentRight.isEmpty()) {
 			return null;
 		}
-		
+
+		// Save block file
+		saveListBlockFile(filename, subFileContentLeft);
+		saveListBlockFile(filename, subFileContentRight);
+
+		// Build the file
 		try {
-			File file = new File( dfsRootFolder + "/" + filename);
+			File file = new File(dfsRootFolder + "/" + filename);
 			file.createNewFile();
 			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
-			
-			for( byte indexPart = 0; indexPart < slaves.length; indexPart++){
-				byte[] data = null;
-				if( indexPart % 2 == 0 ){
-					data = popFileBlockAtIndex(indexPart, subFileContentLeft);
-				}
-				else{
-					data = popFileBlockAtIndex(indexPart, subFileContentRight);
-				}
-				bos.write(data, 1, data.length-1);
+
+			for (byte indexPart = 0; indexPart < slaves.length; indexPart++) {
+				byte[] data = readBlockFile(filename, indexPart);
+				bos.write(data, 1, data.length - 1);
 			}
-			
+
 			bos.close();
 			return file;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
-	 * Search and pop the memory block with index 'indexPart'
-	 * @param indexPart
-	 * @param list
-	 * @return the memory block with indexPart, else null
+	 * Save data blocks
+	 * 
+	 * @param filename:
+	 *            the file's name build with this blocks
+	 * @param list:
+	 *            the block list
 	 */
-	private byte[] popFileBlockAtIndex(byte indexPart, List<byte[]> list){
-		Iterator<byte[]> i = list.iterator();
-		
-		while(i.hasNext()){
-			byte []block = i.next();
-			if( block[0] == indexPart ){
-				i.remove();
-				return block;
+	private void saveListBlockFile(String filename, List<byte[]> list) {
+		for (byte[] block : list) {
+			File file = new File(dfsRootFolder + "/" + filename + "-" + block[0]);
+			saveBlockFile(file, block);
+		}
+	}
+
+	/**
+	 * Allow to save the data block in a file
+	 * 
+	 * @param file
+	 * @param data
+	 */
+	private void saveBlockFile(File file, byte[] data) {
+		BufferedOutputStream bos = null;
+		try {
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			bos = new BufferedOutputStream(new FileOutputStream(file));
+			bos.write(data);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (bos != null) {
+				try {
+					bos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		
+	}
+
+	/**
+	 * Read the file contain the data block at indexPart.
+	 * 
+	 * @param filename
+	 * @param indexPart
+	 * @return
+	 */
+	private byte[] readBlockFile(String filename, int indexPart) {
+		try {
+			File file = new File(dfsRootFolder + "/" + filename + "-" + indexPart);
+			return Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 
@@ -206,54 +250,56 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 	public byte[] retrieveBytes(String filename) throws RemoteException {
 		List<byte[]> subFileContentLeft = slaves[0].subRetrieve(filename);
 		List<byte[]> subFileContentRight = slaves[1].subRetrieve(filename);
-		
-		if( subFileContentLeft.isEmpty() || subFileContentRight.isEmpty() ){
+
+		if (subFileContentLeft.isEmpty() || subFileContentRight.isEmpty()) {
 			return null;
 		}
-		
+
+		// Save block file
+		saveListBlockFile(filename, subFileContentLeft);
+		saveListBlockFile(filename, subFileContentRight);
+
+		// Build the file
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		
-		for( byte indexPart = 0; indexPart < slaves.length; indexPart++){
-			byte[] data = null;
-			if( indexPart % 2 == 0 ){
-				data = popFileBlockAtIndex(indexPart, subFileContentLeft);
-			}
-			else{
-				data = popFileBlockAtIndex(indexPart, subFileContentRight);
-			}
+		for (byte indexPart = 0; indexPart < slaves.length; indexPart++) {
+			byte[] data = readBlockFile(filename, indexPart);
 			baos.write(data, 1, data.length - 1);
 		}
-		
+
 		return baos.toByteArray();
 	}
 
-	/* (non-Javadoc)
-	 * @see fr.unice.miage.sd.tinydfs.nodes.Master#addSlave(fr.unice.miage.sd.tinydfs.nodes.Slave)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.unice.miage.sd.tinydfs.nodes.Master#addSlave(fr.unice.miage.sd.tinydfs
+	 * .nodes.Slave)
 	 */
 	@Override
 	public boolean addSlave(Slave slave) throws RemoteException {
 		int i = 0;
-		for(; i < slaves.length; i++){
-			if( slaves[i] == null ){
+		for (; i < slaves.length; i++) {
+			if (slaves[i] == null) {
 				slaves[i] = slave;
 				break;
 			}
 		}
-		
-		if( i == slaves.length ){
+
+		if (i == slaves.length) {
 			return false;
 		}
-		
-		//create the link with the parent
-		if( i > 1){
-			int indexParent = ( i >> 1 ) - 1; // (i / 2) -1
-			if( (i & 1) == 0 ){ // i is divisible by 2
+
+		// create the link with the parent
+		if (i > 1) {
+			int indexParent = (i >> 1) - 1; // (i / 2) -1
+			if ((i & 1) == 0) { // i is divisible by 2
 				slaves[indexParent].setLeftSlave(slave);
-			}else{
+			} else {
 				slaves[indexParent].setRightSlave(slave);
 			}
 		}
-		
+
 		return true;
 	}
 }
